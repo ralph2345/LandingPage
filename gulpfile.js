21509c22a73be7e3fa568a4c1985ebd11a2b147c
ralph2345 <144690960+ralph2345@users.noreply.github.com>
2025-02-12 16:09:46 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('style.scss')
-        .pipe(sass())
+    return src('scss_files/main.scss') 
+        .pipe(sass().on('error', sass.logError))
         .pipe(dest('css'));
 }
 
 function watchTask() {
-    watch(['style.scss'], buildStyles);
+    watch(['scss_files/**/*.scss'], buildStyles); 
 }
 
 exports.default = series(buildStyles, watchTask);
